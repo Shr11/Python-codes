4f1c986c48e169d9a16e32d2437c1d9fcd86d6b6
MageXstarLynn <shr.comp1@gmail.com>
2023-03-22 11:15:17 +0530

    adding f string to args fn

--- args.py
+++ args.py
@@ -1,14 +1,14 @@
 
 #function definition
 
-def fun( z , a, x= 20, y=10 ):        # x,y are default arguments & z,a are keyword arguments
-    sum = x + y                       # default sum = 30
+def fun( z , a, x= 20, y=10 ):                    # x,y are default arguments & z,a are keyword arguments
+    sum = x + y                                   # default sum = 30
     product = a*z
-    return 'sum is {} & product is {}'.format(sum,product)
+    return f'sum is {sum} & product is {product}' #using f string instead of "{} {}".format(sum,product)
    
    
 #function call   
     
-string = fun(z = 10,a=50)        #But default arg always follows non default arguments in parameters
+string = fun(z = 10,a=50)                         #But default arg always follows non default arguments in parameters
 print(string)
 
